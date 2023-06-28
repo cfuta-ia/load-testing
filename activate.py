f1cce31f0f6f165cbaf5af064c856810999454ac
cfuta-ia <cfuta@inductiveautomation.com>
2023-06-28 16:00:14 -0600

    Commit2

---
+++ activate.py
@@ -0,0 +1,6 @@
+#from framework.app import start_service
+
+# Start Flask server
+if __name__ == '__main__':
+    print('Starting Service')
+    #start_service()
---
+++ framework/app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from flask import request
+import os, signal, datetime
+
+app = Flask(__name__)
